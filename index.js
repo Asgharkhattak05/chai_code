12747a0f0cb257703aab66da8df28465858cd770
Asghar Waheed <arhumkhattak05@gmail.com>
2024-01-26 10:47:59 +0500

    adjust

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT=process.env.PORT || 3000
+import dotenv from 'dotenv';
+import express from 'express';
 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.get("/twitter", (req ,res)=>{
-    res.send("AsgharTwiter.93")
-})
-app.get("/person" , ( req, res ) => {
-    res.send("<h1>Hello Chai our Asghar</h1>")
-})
+app.get("/twitter", (req, res) => {
+  res.send("AsgharTwiter.93");
+});
+
 
 app.listen(PORT, () => {
-  console.log(` App Listening On ${PORT}`)
-})
+  console.log(`App Listening On ${PORT}`);
+});
