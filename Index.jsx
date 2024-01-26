5a75f26eb049fc1f9202ac120a80966faba3fa49
Asghar Waheed <arhumkhattak05@gmail.com>
2024-01-26 09:40:01 +0500

    add port

--- Index.jsx
+++ Index.jsx
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
+const PORT=process.env.PORT || 3000
 
 
 app.get('/', (req, res) => {
@@ -15,6 +15,6 @@ app.get("/person" , ( req, res ) => {
     res.send("<h1>Hello Chai our Asghar</h1>")
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(` App Listening On`)
+app.listen(PORT, () => {
+  console.log(` App Listening On ${PORT}`)
 })
